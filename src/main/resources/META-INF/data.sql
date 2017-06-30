23e0e240f7e1224fe5668bd4cdf955f9e6ace1a5
alsem <av.semennikov@gmail.com>
2017-06-30 22:01:47 +0300

    removed unused classes

--- src/main/resources/META-INF/data.sql
+++
@@ -1,5 +0,0 @@
-
-INSERT INTO public.node (node_id, name, region, street, housenumber) VALUES (nextval('node_id_seq'), 'Baldur', 'Kopenhagen', 'Sankt Peders Straade', '56');
-INSERT INTO public.node (node_id, name, region, street, housenumber) VALUES (nextval('node_id_seq'), 'Bragi', 'Kopenhagen', 'Fensmarkgade', '34');
-INSERT INTO public.node (node_id, name, region, street, housenumber) VALUES (nextval('node_id_seq'), 'Kvasir', 'Kopenhagen', 'Adelgade', '34');
-INSERT INTO public.node (node_id, name, region, street, housenumber) VALUES (nextval('node_id_seq'), 'Loki', 'Kopenhagen', 'Borgergade', '38');
